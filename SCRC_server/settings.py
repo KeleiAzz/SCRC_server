fa6a59cdad1fc6260cbf8a7d94cf999b713351d1
KeleiAzz <kgong@ncsu.edu>
2015-05-20 16:13:52 -0400

    forget to add changes

--- SCRC_server/settings.py
+++ SCRC_server/settings.py
@@ -87,6 +87,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
 )
+# TEMPLATE_DIRS = (
+#     os.path.join(BASE_DIR,  'templates'),
+# )
--- query/views.py
+++ query/views.py
@@ -11,8 +11,11 @@ def home_page(request):
     return render(request, 'home.html', {'companies': companies})
 
 def company_details(request, id):
-    company = Company.objects.get(id=id)
-    ratings = Rating.objects.all().filter(company_id=id).order_by('date', 'category_id')
+    if len(id) > 3:
+        company = Company.objects.get(name=id)
+    else:
+        company = Company.objects.get(id=id)
+    ratings = Rating.objects.all().filter(company_id=company.id).order_by('date', 'category_id')
 
     return render(request, 'company_details.html', {'company': company, 'ratings': ratings})
 
@@ -35,28 +38,44 @@ def advanced_search(request):
     if 'btn1' in request.POST:
         form = MultipleChoiceForm(request)
         if request.POST.get('selected', 0) != 0:
-            selected = request.POST.get('selected').split('#')
-            selected = list(set(selected))
+            selected_company = request.POST.get('selected').split('#')
+            selected_company = list(set(selected_company))
             # selected_company = [i for i in selected]
-
+            selected_company_name = [Company.objects.get(id=int(i)).name for i in selected_company]
+            selected_details = {}
+            for i in range(len(selected_company)):
+                selected_details[selected_company_name[i]] = \
+                    Rating.objects.all().filter(company_id=int(selected_company[i])).order_by('date','category_id')
         return render(request, 'advanced_search.html', {'form': form,
                                                         'choice_field': request.POST.getlist('choice_field', 0),
-                                                        'selected_company_names': '#'.join(selected),
-                                                        'selected_company': selected,})
+                                                        'selected_company_id': '#'.join(selected_company),
+                                                        'selected_company_name': selected_company_name,
+                                                        'selected_details': selected_details
+                                                        })
 
     if 'btn2' in request.POST:
         if request.POST.get('selected', 0) != 0:
             selected = request.POST.get('selected').split('#')
-            selected_company = [i for i in selected]
+            selected_company = selected
+
 
         if request.method == 'POST':
-            selected_company += [Company.objects.get(id=i).name for i in request.POST.getlist('choice_field')]
+            selected_company += [str(i) for i in request.POST.getlist('choice_field')]
             selected_company = list(set(selected_company))
-            selected_company_names = '#'.join(selected_company)
+            if '' in selected_company:
+                selected_company.remove('')
+            selected_company_id = '#'.join(selected_company)
+            selected_company_name = [Company.objects.get(id=int(i)).name for i in selected_company]
+            selected_details = {}
+            for i in range(len(selected_company)):
+                selected_details[selected_company_name[i]] = \
+                    Rating.objects.all().filter(company_id=int(selected_company[i])).order_by('date','category_id')
 
             return render(request, 'advanced_search.html', {'form': form,
-                                                            'selected_company': selected_company,
-                                                            'selected_company_names': selected_company_names,})
+                                                            'selected_company_name': selected_company_name,
+                                                            'selected_details': selected_details,
+                                                            'selected_company_id': selected_company_id,
+                                                            })
 
     # if request.method == 'POST' and request.POST.getlist('choice_field', 0) == 0 :
     #     companies = Company.objects.filter(name__icontains=request.POST['company_name'])
