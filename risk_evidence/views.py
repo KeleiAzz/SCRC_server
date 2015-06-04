910b53838530aeb120fa269fe847fcd5cffa0dbd
KeleiAzz <kgong@ncsu.edu>
2015-06-04 13:55:59 -0400

    Re-structure the display of score list, divided by two major category

--- risk_evidence/views.py
+++ risk_evidence/views.py
@@ -18,7 +18,9 @@ def home(request):
 
 def score_list(request):
     scores = Score.objects.all()
-    return render(request, 'score_list.html', {'scores': scores, 'form': ScoreForm})
+    scores1 = Score.objects.all().filter(category='SCI').order_by('sub_category')
+    scores2 = Score.objects.all().filter(category='P').order_by('sub_category')
+    return render(request, 'score_list.html', {'scores1': scores1, 'form': ScoreForm, 'scores2': scores2})
 
 def score_add(request):
 
@@ -27,15 +29,18 @@ def score_add(request):
         form.save()
         scores = Score.objects.all()
         return redirect('score_list')
-    scores = Score.objects.all()
-    return render(request, 'score_add.html', {'scores': scores, 'form': ScoreForm})
+    scores1 = Score.objects.all().filter(category='SCI').order_by('sub_category')
+    scores2 = Score.objects.all().filter(category='P').order_by('sub_category')
+    return render(request, 'score_add.html', {'scores1': scores1, 'form': ScoreForm, 'scores2': scores2})
 
 def score_edit(request, score_id):
     scores = Score.objects.all()
+    scores1 = Score.objects.all().filter(category='SCI').order_by('sub_category')
+    scores2 = Score.objects.all().filter(category='P').order_by('sub_category')
     instance = get_object_or_404(Score, id=score_id)
     form = ScoreForm(request.POST or None, instance=instance)
     if form.is_valid():
         form.save()
         return redirect('score_list')
-    return render(request, 'score_edit.html', {'scores': scores,'form': form, "score_id": instance.id})
+    return render(request, 'score_edit.html', {'scores1': scores1, 'scores2': scores2, 'form': form, "score_id": instance.id})
 
