d4de752662f1d7d27c778c763bdb67441f27729d
KeleiAzz <kgong@ncsu.edu>
2015-06-04 14:55:56 -0400

    Create model evidence, seems not in a proper way

--- risk_evidence/models.py
+++ risk_evidence/models.py
@@ -30,3 +30,41 @@ class Score(models.Model):
     def get_absolute_url(self):
         return reverse('score_edit', kwargs={'pk': self.pk})
 
+
+
+class Evidence(models.Model):
+    name = models.TextField(default='')
+    note = models.TextField(default='')
+    link = models.TextField(default='')
+    country = models.CharField(max_length=50)
+    type = models.CharField(max_length=30)
+    CATEGORY_CHOICES = (
+        ('SCI', 'Supply Chain Impact'),
+        ('P', 'Probability'),
+    )
+    category = models.CharField(max_length=5, choices=CATEGORY_CHOICES)
+    credibility = models.ForeignKey(Score, limit_choices_to={'sub_category': 'Credibility'}, related_name='+')
+    revelance = models.ForeignKey(Score, limit_choices_to={'sub_category': 'Revelance'}, related_name='+')
+    h1 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h2 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h3 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h4 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h5 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h6 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h7 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h8 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h9 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h10 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h11 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h12 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h13 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h14 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h15 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h16 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h17 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h18 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h19 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h20 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h21 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h22 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
+    h23 = models.ForeignKey(Score, limit_choices_to={'sub_category': None}, related_name='+')
