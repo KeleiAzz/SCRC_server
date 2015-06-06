33ae09c8e0efede5606c456a699bb634777c3b66
KeleiAzz <kgong@ncsu.edu>
2015-06-06 00:54:19 -0400

    Add view to display probability list

--- risk_evidence/urls.py
+++ risk_evidence/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^score/list/', 'risk_evidence.views.score_list', name='score_list'),
     url(r'^score/add', 'risk_evidence.views.score_add', name='add_score'),
     url(r'^score/edit/(\d+)/$', 'risk_evidence.views.score_edit', name='edit_score'),
-    url(r'^evidence/list/', 'risk_evidence.views.evidence_list', name='evidence_list')
+    url(r'^supply_chain_impact/list/', 'risk_evidence.views.sci_list', name='sci_list'),
+    url(r'^probability/list/', 'risk_evidence.views.probability_list', name='probability_list')
 )
