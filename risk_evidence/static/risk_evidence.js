a8a0f277fe3b72e53e5271af916c4d315a94b425
KeleiAzz <kgong@ncsu.edu>
2015-06-14 15:53:10 -0400

    Add some js code to auto select country and hide error message

--- risk_evidence/static/risk_evidence.js
+++ risk_evidence/static/risk_evidence.js
@@ -33,3 +33,47 @@ jQuery(document).ready(function(){
     });
 });
 
+//$('#left').find(".form-country").on('change',function(){
+//    $("#id_country option:contains('China')").attr('selected', true);;
+//});
+//$('#right').find(".form-country").on('change',function(){
+//    $('#id_summary').val("hahah");
+//
+//});
+
+jQuery(document).ready(function () {
+    $('.form-control2').on('click', function() {
+        $('.errorlist').hide();
+    });
+    $('.form-control3').on('click', function() {
+        $('.errorlist').hide();
+    });
+});
+
+function sync(el1, el2) {
+    if (!el1) {
+        return false;
+    }
+    else {
+        var val = el1.value;
+        var syncWith = document.getElementById(el2);
+        var options = syncWith.getElementsByTagName('option');
+        for (var i = 0, len = options.length; i < len; i++) {
+            if (options[i].value == val) {
+                options[i].selected = true;
+            }
+        }
+    }
+}
+
+var selectToSync = document.getElementById('id_form1-country');
+selectToSync.onchange = function(){
+    sync(this,'id_form2-country');
+};
+
+var selectToSync2 = document.getElementById('id_form2-country');
+selectToSync2.onchange = function(){
+    sync(this,'id_form1-country');
+};
+
+
