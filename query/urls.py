5103315ff936d6c5d3e7b960e1181b42b96037c9
KeleiAzz <kgong@ncsu.edu>
2015-08-22 14:45:22 -0400

    enable query site

--- query/urls.py
+++ query/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'SCRC_server.views.home', name='home'),
+    url(r'^$', 'query.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^$', 'query.views.home_page', name='home'),
     url(r'^companies/(.+)/$', 'query.views.company_details', name='company_details'),
